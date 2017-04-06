c456261cc31f9eb1afd80dad3cbca2433145ba0f
Riley Smith <riley.smith@sea.aie.edu>
2017-04-05 18:46:22 -0700

    Movement Prototype working

---
+++ Assets/BoostTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform _cameraTransform;
+    public Transform CameraTransform
+    {
+        get
+        {
+            if (_cameraTransform == null)
+                _cameraTransform = GameObject.Find("Camera (head)").transform;
+
+            return _cameraTransform;
+        }
+    }
+    private Vector3 CameraFace2DAxis;
+
+    public Transform CameraRigTransform;
+
+    [SerializeField]
+    private ControllerHandler controller;
+
+    SteamVR_Controller.Device device;
+
+    bool Boost;
+    Vector3 lerpToPosition;
+
+    void Update()
+    {
+        transform.position = new Vector3(CameraTransform.position.x, transform.position.y, CameraTransform.position.z);
+        CameraFace2DAxis = new Vector3(CameraTransform.forward.x, 0, CameraTransform.forward.z);
+        Debug.DrawLine(CameraTransform.position, CameraTransform.position + CameraFace2DAxis);
+
+        if (controller.CirclePadValue != Vector2.zero)
+            Boost = true;
+        else
+            Boost = false;
+
+        Vector3 vel = new Vector3();
+        if (Boost)
+            lerpToPosition = CameraRigTransform.position + CameraFace2DAxis.normalized;
+
+        CameraRigTransform.position = Vector3.SmoothDamp(CameraRigTransform.position, lerpToPosition, ref vel, .075f);
+    }
+}
---
+++ Assets/ControllerHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerHandler : MonoBehaviour
+{
+    public bool triggerButtonDown = false;
+    public bool dLeftDown = false;
+    public bool dRightDown = false;
+    public bool gripped = false;
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private Valve.VR.EVRButtonId dLeft = Valve.VR.EVRButtonId.k_EButton_DPad_Left;
+    private Valve.VR.EVRButtonId dRight = Valve.VR.EVRButtonId.k_EButton_DPad_Right;
+    private Valve.VR.EVRButtonId grip = Valve.VR.EVRButtonId.k_EButton_Grip;
+
+    float triggerBuffer = .5f;
+    public bool canTrigger;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    public Vector2 CirclePadValue;
+
+    void Start()
+    {
+
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+    {
+
+        triggerBuffer -= Time.deltaTime;
+        if (triggerBuffer <= 0)
+        {
+            canTrigger = true;
+        }
+        else { canTrigger = false; }
+        //lr.SetPositions(transform.position, po.objToPlace.transform.position);
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+        dLeftDown = controller.GetPress(dLeft);
+        dRightDown = controller.GetPress(dRight);
+        gripped = controller.GetPressDown(grip);
+        
+        CirclePadValue = controller.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
+        Debug.Log(CirclePadValue);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward);
+    }
+}
+
